5fcc288f188e0a2915838cd466555b2cba728eb4
Yufan-l <Yufan-l@users.noreply.github.com>
2016-09-02 18:18:17 -0700

    make states file dir configurable

--- org.cg.impala.streaming/docker/server-start.sh
+++ org.cg.impala.streaming/docker/server-start.sh
@@ -16,7 +16,7 @@ sed -i -- "s|{{service_port}}|$service_port|g" $CONFIG
 sed -i -- "s|{{impala_url}}|$impala_url|g" $COMPACTION_CONF
 sed -i -- "s|{{tmp_table_dir}}|$tmp_table_dir|g" $COMPACTION_CONF
 sed -i -- "s|{{hdfs_url}}|$hdfs_url|g" $COMPACTION_CONF
-
+sed -i -- "s|{{stateFilesDir}}|$stateFilesDir|g" $COMPACTION_CONF
 
 
 
